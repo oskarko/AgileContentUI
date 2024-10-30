eb2542aec8f15651d27c592e1b50dbbf07452a90
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2024-10-30 14:09:58 +0100

    Initial commit

---
+++ AgileContentUI/AgileContentUIApp.swift
@@ -0,0 +1,19 @@
+//
+//  AgileContentUIApp.swift
+//  AgileContentUI
+//
+//  Created by Oscar Rodriguez Garrucho on 30/10/24
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2024 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AgileContentUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
